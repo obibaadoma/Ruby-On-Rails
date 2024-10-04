6dfca4c98a1a90b0db2550fcebd81d680b063feb
Daa Nkwa <akrasikonadu@yahoo.com>
2024-10-04 12:40:36 +0000

    add array

--- student.rb
+++ student.rb
@@ -1,26 +1,18 @@
 class Student
+  attr_accessor :name, :email, :contact
 def initialize(name, email, contact)
 @name = name
 @email = email
 @contact = contact
 end
-
-def print_name
-  puts "Name: #{@name}"
-end
-
-def print_email
-  puts "Email: #{@email}"
-end
-
-def print_contact
-  puts "Contact: #{@contact}"
-
-end
 end
 
 s1 = Student.new('Derek','akrasi@hot.com','123123123')
 
-s1.print_name
-s1.print_email
-s1.print_contact
+s1.name = 'Alex'
+s1.email = 'obiba@whatever.com'
+s1.contact = '123123123124'
+
+puts s1.name
+puts s1.email
+puts s1.contact
